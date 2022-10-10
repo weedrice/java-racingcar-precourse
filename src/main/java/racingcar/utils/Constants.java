def0c7f247914bc402cb4590ff850a0bad1d2d82
STO440 <supportyoo@sk.com>
2022-10-10 17:14:00 +0900

    feat(Constants): 사용 변수 생성

---
+++ src/main/java/racingcar/utils/Constants.java
@@ -0,0 +1,13 @@
+package racingcar.utils;
+
+public class Constants {
+    public static final Integer CAR_NAME_MAX_LENGTH = 5;
+    public static final Integer DEFAULT_CAR_POSITION = 0;
+    public static final Integer RANDOM_MIN_NUMBER = 0;
+    public static final Integer RANDOM_MAX_NUMBER = 9;
+    public static final Integer CAR_MOVING_CONDITION = 4;
+    public static final Integer CAR_MOVING_AMOUNT = 1;
+
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String ERROR_ILLEGAL_CAR_NAME = "자동차 이름은 빈 값 일 수 없고, 5자를 초과할 수 없습니다.";
+}
